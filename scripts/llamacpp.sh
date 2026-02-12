79a9eb37acc7f1dfefbbfc5c1f7763564b64d8d3
JHIH-SIOU LI <jsli@linux.com>
2026-02-13 00:05:42 +0800

    chore: try to use llama.cpp

---
+++ scripts/llamacpp.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+llama-server \
+    -hf unsloth/Qwen3-Coder-Next-GGUF:UD-Q4_K_XL \
+    --port 8001 \
+    --ctx-size 32072 \
+    --ubatch-size 4096 \
+    --batch-size 4096 \
+    --flash-attn on \
+    --fit on \
+    --seed 3407 \
+    --temp 1.0 \
+    --top-p 0.95 \
+    --min-p 0.01 \
+    --top-k 40 \
+    --jinja --ctx-checkpoints 128
